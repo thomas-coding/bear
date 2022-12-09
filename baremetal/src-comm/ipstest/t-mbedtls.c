383fca338b15b9a7ac9d860870a35fea29c79566
Jinping Wu <jinping.wu@verisilicon.com>
2022-12-09 09:58:43 +0800

    mbedtls: refine, not use libc std function

--- baremetal/src-comm/ipstest/t-mbedtls.c
+++ baremetal/src-comm/ipstest/t-mbedtls.c
@@ -37,30 +37,40 @@ exit:
 
 void mbedtls_hash_md5_test(void)
 {
-	int i, ret;
-	unsigned char digest[16];
-	char str[] = "Hello, world!";
+    int i, ret;
+    unsigned char digest[16];
+    char str[] = "Hello, world!";
 
-	vs_printf( "\n  MD5('%s') = ", str );
+    mbedtls_printf( "\n  MD5('%s') = ", str );
 
-	if ( ( ret = mbedtls_md5( (unsigned char *)str, 13, digest ) ) != 0 ) {
-		vs_printf( "md5 fail\n" );
-		return;
-	}
+    if( ( ret = mbedtls_md5( (unsigned char *) str, 13, digest ) ) != 0 )
+        mbedtls_exit( MBEDTLS_EXIT_FAILURE );
 
-	for ( i = 0; i < 16; i++ )
-		vs_printf( "%02x", digest[i] );
+    for( i = 0; i < 16; i++ )
+        mbedtls_printf( "%02x", digest[i] );
 
-	vs_printf( "\n\n" );
+    mbedtls_printf( "\n\n" );
+
+    mbedtls_exit( MBEDTLS_EXIT_SUCCESS );
 }
 
 void mbedtls_hash_test(void)
 {
 	mbedtls_hash_md5_test();
 	mbedtls_hash_md5_part_test();
+	mbedtls_md5_self_test(1);
+}
+
+void mbed_exit(int status) {
+	if(status != MBEDTLS_EXIT_SUCCESS) {
+		vs_printf( "mbed_exit fail, block here\n" );
+		while(1) {};
+	}
 }
 
 void mbedtls_test(void)
 {
+	mbedtls_platform_set_printf((int (*)(const char *, ...))console_printf);
+	mbedtls_platform_set_exit(mbed_exit);
 	mbedtls_hash_test();
 }
